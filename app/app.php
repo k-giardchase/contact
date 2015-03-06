23a614419e559e5e20db3b743f20d1fb9401f4ae
Kyle Giard-Chase <kgiardch@gmail.com>
2015-03-06 12:09:43 -0800

    Fix spacing and formatting.

--- app/app.php
+++ app/app.php
@@ -3,15 +3,17 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/contact.php";
 
-    $app = new Silex\Application();
-
     session_start();
+
     if (empty($_SESSION['list_of_contacts'])) {
         $_SESSION['list_of_contacts'] = array();
     }
 
+    $app = new Silex\Application();
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array (
         'twig.path' => __DIR__.'/../views'
+
     ));
 
     $app->get("/", function() use ($app) {
